1b0dbb598f711404c6515cc1b11951170deef93c
Meng Xea <mengxea@Mengs-MacBook-Pro.local>
2024-11-28 22:51:44 +0700

    implement the cart section , add to cart and remove from cart feature

--- javascript/scripts/product.js
+++ javascript/scripts/product.js
@@ -1,5 +1,7 @@
 import { discountProducts } from "../Data/discount-product-page.js";
 import { smallDiscounts } from "../Data/smallDiscount.js";
+import { departments, prices, discounts } from "./productFilter.js";
+import { products } from "../Data/productsData.js";
 const containerEl = document.querySelector(".discount-cards-container");
 let productStructure = "";
 discountProducts.forEach((product) => {
@@ -71,3 +73,201 @@ preSmallDiscount.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// filter function
+
+let departmentFilterEle = document.querySelector(".Departments-filter > ul");
+let priceFilterEle = document.querySelector(".price-filter > ul");
+let discountFilterEle = document.querySelector(".discount-filter > ul");
+
+let departmentHtml = "";
+departments.forEach((department) => {
+  departmentHtml += `
+      <li><input type="radio" name="department-filter" id="${department}"/> <label for="${department}">${department}</label></li>
+  `;
+});
+departmentFilterEle.innerHTML = departmentHtml;
+
+let priceHtml = "";
+prices.forEach((price) => {
+  priceHtml += `
+      <li><input type="radio" name="price-filter" id="${price}" /><label for="${price}">${price}</label></li>
+  `;
+});
+priceFilterEle.innerHTML = priceHtml;
+
+let discountHtml = "";
+discounts.forEach((discount) => {
+  discountHtml += `
+      <li><input type="radio" name="discount-filter" id="${discount}"/><label for="${discount}">$${discount}</label></li>
+  `;
+});
+discountFilterEle.innerHTML = discountHtml;
+
+//product display
+let productDisplayContainerEle = document.querySelector(".product-display");
+let proDisplayStructure = "";
+products.forEach((product) => {
+  if (product.discountEvent === "") {
+    proDisplayStructure += `
+    <div class="product-card">
+          <div class="image">
+            <img src="${product.img}" alt="" />
+          </div>
+          <div class="discount">
+            <span class="off">${product.discount}</span>
+          </div>
+          <div class="decribe"><p>${product.discription}</p></div>
+          <div class="rated">
+            <div class="rate"><img src="../asset/ratting/rating-${product.rating.stars}.png" alt="" /></div>
+            <div class="user-rate">${product.rating.userRate}</div>
+          </div>
+          <div class="prices">${product.price} <span class="small-text-price">${product.pricePoint}</span></div>
+          <div class="shipping">shipping to cambodia</div>
+          <button class="add-to-cart js-add-to-card" data-product-id="${product.id}">Add to cart</button>
+    </div>
+  `;
+  } else if (product.discount === "") {
+    proDisplayStructure += `
+    <div class="product-card">
+          <div class="image">
+            <img src="${product.img}" alt="" />
+          </div>
+          <div class="decribe"><p>${product.discription}</p></div>
+          <div class="rated">
+            <div class="rate"><img src="../asset/ratting/rating-${product.rating.stars}.png" alt="" /></div>
+            <div class="user-rate">${product.rating.userRate}</div>
+          </div>
+          <div class="prices">${product.price} <span class="small-text-price">${product.pricePoint}</span></div>
+          <div class="shipping">shipping to cambodia</div>
+          <button class="add-to-cart js-add-to-card" data-product-id="${product.id}">Add to cart</button>
+    </div>
+  `;
+  } else if (product.price === "") {
+    proDisplayStructure += `
+    <div class="product-card">
+          <div class="image">
+            <img src="${product.img}" alt="" />
+          </div>
+          <div class="discount">
+            <span class="off">${product.discount}</span>
+            <span class="event">${product.discountEvent}</span>
+          </div>
+          <div class="decribe" data-product-id="${product.id}"><p>${product.discription}</p></div>
+          <div class="rated">
+            <div class="rate"><img src="../asset/ratting/rating-${product.rating.stars}.png" alt="" /></div>
+            <div class="user-rate">${product.rating.userRate}</div>
+          </div>
+    </div>
+  `;
+  } else if (product.rating === undefined) {
+    proDisplayStructure += `
+    <div class="product-card">
+          <div class="image">
+            <img src="${product.img}" alt="" />
+          </div>
+          <div class="discount">
+            <span class="off">${product.discount}</span>
+            <span class="event">${product.discountEvent}</span>
+          </div>
+          <div class="decribe"><p>${product.discription}</p></div>
+          <div class="prices">${product.price} <span class="small-text-price">${product.pricePoint}</span></div>
+          <div class="shipping">shipping to cambodia</div>
+          <button class="add-to-cart js-add-to-card" data-product-id="${product.id}">Add to cart</button>
+    </div>
+  `;
+  } else {
+    proDisplayStructure += `
+    <div class="product-card">
+          <div class="image">
+            <img src="${product.img}" alt="" />
+          </div>
+          <div class="discount">
+            <span class="off">${product.discount}</span>
+            <span class="event">${product.discountEvent}</span>
+          </div>
+          <div class="decribe"><p>${product.discription}</p></div>
+          <div class="rated">
+            <div class="rate"><img src="../asset/ratting/rating-${product.rating.stars}.png" alt="" /></div>
+            <div class="user-rate">${product.rating.userRate}</div>
+          </div>
+          <div class="prices">${product.price} <span class="small-text-price">${product.pricePoint}</span></div>
+          <div class="shipping">shipping to cambodia</div>
+          <button class="add-to-cart js-add-to-card" data-product-id="${product.id}">Add to cart</button>
+    </div>
+  `;
+  }
+});
+productDisplayContainerEle.innerHTML = proDisplayStructure;
+// end product display
+
+// add products to cart
+const carts = [];
+let cartsItmes = document.querySelector(".cart-quantity");
+cartsItmes.innerText = 0;
+const addToCartBtnEle = document.querySelectorAll(".js-add-to-card");
+addToCartBtnEle.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    cartsItmes.innerText++;
+    const matchedProduct = products.find((product) => product.id === btn.dataset.productId);
+    if (matchedProduct) {
+      carts.push(matchedProduct);
+    }
+    renderCarts();
+  });
+});
+
+// carts
+function updateAboutOfItems() {
+  let amountItemsEle = document.querySelector(".amount-number");
+  const increaseBtnEle = document.querySelector(".increase-btn");
+  const decreaseBtnEle = document.querySelector(".descrease-btn");
+
+  amountItemsEle.innerText = 0;
+
+  increaseBtnEle.addEventListener("click", () => {
+    amountItemsEle.innerText++;
+  });
+  decreaseBtnEle.addEventListener("click", () => {
+    if (amountItemsEle.innerText > 0 || amountItemsEle.innerText < "0") {
+      amountItemsEle.innerText--;
+    }
+  });
+}
+
+function renderCarts() {
+  let cartStructure = " ";
+  carts.forEach((cart) => {
+    let short_description = cart.discription.split(" ").slice(0, 8).join(" ");
+    cartStructure += `
+      <div class="carts-card">
+        <div class="image">
+          <img src="${cart.img}" alt="" />
+        </div>
+        <div class="product-text">${short_description}</div>
+        <div class="amount">
+          <span class="amount-number"></span>
+          <div class="control-amount-btn">
+            <div class="increase-btn"><i class="fa-solid fa-caret-up"></i></div>
+            <div class="descrease-btn"><i class="fa-solid fa-caret-down"></i></div>
+          </div>
+        </div>
+        <div class="pay"><button class="pay-btn">pay</button><button class="delete-btn" data-product-id="${cart.id}"}>Remove</button></div>
+      </div>
+  `;
+  });
+  document.querySelector(".carts-display-container").innerHTML = cartStructure;
+  updateAboutOfItems();
+  removeFromCart();
+}
+
+function removeFromCart() {
+  let removeProductBtnEle = document.querySelectorAll(".delete-btn");
+  removeProductBtnEle.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      carts.pop();
+      cartsItmes.innerText--;
+      renderCarts();
+    });
+  });
+}
---
+++ javascript/scripts/productFilter.js
@@ -0,0 +1,22 @@
+const departments = [
+  "All",
+  "Arts Crafts & Sewing",
+  "Automotive",
+  "Baby products",
+  "Beauty & Personal care",
+  "Electronics",
+  "Grocery",
+  "Health & household",
+  "Home & Kitchen",
+  "Kindle Store",
+  "Movies TV",
+  "Pet supplies",
+  "Toys and Games",
+  "Video game",
+];
+
+const prices = ["All", "Under $25", "Under $50", "Under $100", "Under $200", "$200 & Above"];
+
+const discounts = ["All", "10% off or more", "25% off or more", "50% off and more", "70% off or more"];
+
+export { departments, prices, discounts };
